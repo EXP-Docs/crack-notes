ad2850adfaff3527b8bc93ebf43ea298be0c08a6
EXP <289065406@qq.com>
2021-03-06 20:09:00 +0800

    init

---
+++ challenge/吾爱破解 2016 安全挑战赛/README.md
@@ -0,0 +1,2 @@
+https://www.52pojie.cn/thread-480746-1-1.html
+
---
+++ tools/02_dbg/Ollydbg/plugin/minimalist-radasm/vc98/include/inc.h
@@ -0,0 +1,10 @@
+#include <windows.h>
+#include <commdlg.h>
+#include <commctrl.h>
+#include <ole2.h>
+#include <richedit.h>
+#include <shellapi.h>
+#include <shlwapi.h>
+#include <winver.h>
+
+#define _N __declspec (naked)
